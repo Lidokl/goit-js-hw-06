8590f0294d8180e732ecea2306c2840f5054457a
Lidija Levchenko <levchenkola@ukr.net>
2021-12-04 21:26:33 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,26 @@
-const listRef = document.querySelectorAll(".item");
+const listEl = document.querySelectorAll(".item");
 const numCategories = function () {
-  return `Number of categories:${listRef.length}`;
+  return `Number of categories:${listEl.length}`;
 };
 console.log(numCategories());
-// --------------------------------------------------------------
-const listCategory = listRef.forEach((element) => {
+
+const listCategory = listEl.forEach((element) => {
   console.log(`Category:${element.firstElementChild.textContent}`);
   console.log(`Elements:${element.lastElementChild.children.length}`);
 });
+
+
+
+
+
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
